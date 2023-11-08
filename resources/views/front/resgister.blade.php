11a30caf1fddd89cb148c175e0347decf774bd65
Murad <agamedov94@mail.ru>
2023-11-08 18:09:31 +0400

    commit1

--- resources/views/front/resgister.blade.php
+++ resources/views/front/resgister.blade.php
@@ -52,7 +52,7 @@
                                         <div class="mx-4 mb-3 mb-lg-4">
 
                                             <button type="button" class="btn btn-primary btn-lg">
-                                                Register
+                                                Registerr
                                             </button>
                                             <p class="small fw-bold mt-2 pt-1 mb-0">
                                                 Have an account?
